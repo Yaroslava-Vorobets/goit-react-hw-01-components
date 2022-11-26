ea7f0f5ee10e65daa93e9e92fe15e49a601078f2
Yaroslava-Vorobets <slavavor85@gmail.com>
2022-11-26 08:51:41 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,28 +6,25 @@ import FriendsList from '../Friends/FriendList';
 import friends from '../Friends/friends.json';
 import TransactionHistory from '../Transaction/TransactionHistory';
 import transactions from '../Transaction/transactions.json';
-// import GlobalStyle from '../GlobajStyles';
 import 'modern-normalize';
 
 export const App = () => {
   return (   
-    <> 
-        {/* <GlobalStyle/> */}
-          <Profile
-            username={user.username}
-            tag={user.tag}
-            location={user.location}
-            avatar={user.avatar}
-            stats={user.stats}
-            statsFollow={user.stats.followers}
-            statsView={user.stats.views} 
-            statsLikes={user.stats.likes}        
-          />     
-          <Statistics title="Upload stats" ststs={data}/> 
-          <FriendsList friends={friends}/> 
-      <TransactionHistory items={transactions} />
-    
-      </>     
+    <>     
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        statsFollow={user.stats.followers}
+        statsView={user.stats.views} 
+        statsLikes={user.stats.likes}        
+      />     
+      <Statistics title="Upload stats" ststs={data}/> 
+      <FriendsList friends={friends}/> 
+      <TransactionHistory items={transactions}/>    
+  </>     
       );
 }
     
